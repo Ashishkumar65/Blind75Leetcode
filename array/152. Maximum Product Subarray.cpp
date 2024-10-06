cad85379ee27f8acf27a20ee0330d28e3fad91fd
ASHISH KUMAR <83687087+Ashishkumar65@users.noreply.github.com>
2024-10-06 23:16:24 +0530

    Update 152. Maximum Product Subarray.cpp

--- array/152. Maximum Product Subarray.cpp
+++ array/152. Maximum Product Subarray.cpp
@@ -8,7 +8,7 @@ public:
         int ans = INT_MIN;
         for(int i=0; i<n; i++){
             if(nums[i] < 0){
-                swap(maxProduct, minProduct);
+                swap(maxProduct, minProduct);  // if there is even number of -ve values we need to have product of all num present in nums if there is no zero.
             }
             maxProduct = max(maxProduct*nums[i], nums[i]);
             minProduct = min(minProduct*nums[i], nums[i]);
